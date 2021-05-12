6ea8b90b6336927144d70a8f492d9cbbfa770d64
hardik281199 <dobariyahardik000@gmail.com>
2021-05-12 19:45:40 +0530

    create-registration-and-login complate

--- WEEK2/Day2/project/index.js
+++ WEEK2/Day2/project/index.js
@@ -4,6 +4,8 @@ const path = require('path');
 const ejs = require('ejs');
 var app = express();
 const bodyParser = require('body-parser');
+const session = require('express-session');
+var flash = require('express-flash');
 
 // app.use(bodyparser.json());
 
@@ -41,31 +43,108 @@ app.set('views',path.join(__dirname,'views'));
 //set view engine
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(flash());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
 
 /**
  * get all element
  */
-app.get('/',(req, res) => {
-    // res.send('CRUD Operation using NodeJS / ExpressJS / MySQL');
-    let sql = "SELECT * FROM employee";
-    let query = mysqlConn.query(sql, (err, rows) => {
-        if(err) throw err;
-        res.render('user_index', {
-            title : 'CRUD Operation using NodeJS'+' '+ formatAMPM(),
-            employee : rows
-        });
+ app.get('/login',(req, res) => {
+    res.render('login');
+});
+
+app.get('/register',(req, res) => {
+    res.render('register');
+});
+
+app.post('/auth', function(req, res) {
+	var username = req.body.username;
+	var password = req.body.password;
+	if (username && password) {
+		mysqlConn.query('SELECT * FROM user WHERE email = ? AND password = ?', [username, password], function(err,rows, fields) {
+			
+            if (rows.length <= 0) {
+                req.flash('error', 'Please correct enter email and Password!')
+                res.redirect('/login')
+            }
+            else { 
+                req.session.loggedin = true;
+                req.session.username = username;
+                req.flash('success', 'Logged In Successfully')
+                res.redirect('/');
+            }            
+		});
+	} else {
+        req.flash('error', 'Please provide username and password')
+        res.redirect("/login");
+		
+	}
+});
+
+app.post('/register',(req,res) => {
+    inputData = {email :req.body.email,password :req.body.psw};
+    passwordrep =req.body.pswrepeat;
+    var sql='SELECT * FROM user WHERE email =?';
+    mysqlConn.query(sql, [inputData.email] ,function (err, data, fields) {
+        if(err) throw err
+        console.log("hii" + data.length);
+        if(data.length>0) {
+            req.flash('error',`email was already exist`);
+            res.redirect("/register");
+        } else if(inputData.password != passwordrep){
+            req.flash('error','Password & Confirm Password is not Matched');
+            res.redirect("/register");
+        } else{
+
+            // save users data into database
+            var sql = 'INSERT INTO user SET ?';
+            mysqlConn.query(sql, inputData, function (err, data) {
+            if (err) throw err;
+                });
+            req.flash('success','Your are successfully registered');
+            res.redirect("/register");
+        }
     });
+    
+});
+
+
+app.get('/',(req, res) => {
+    
+    if (req.session.loggedin){
+        let sql = "SELECT * FROM employee";
+        let query = mysqlConn.query(sql, (err, rows) => {
+            if(err) throw err;
+            res.render('user_index', {
+                title : 'CRUD Operation using NodeJS'+' '+ formatAMPM(),
+                employee : rows
+            });
+        });
+    } else {
+		res.redirect('/login');
+	}
+	// res.end();
+    
 });
 
 app.get('/add',(req, res) => {
-    res.render('user_add', {
-        title : 'CRUD Operation using NodeJS'+' '+ formatAMPM(),
-    });
+    if (req.session.loggedin){
+        res.render('user_add', {
+            title : 'CRUD Operation using NodeJS'+' '+ formatAMPM(),
+        });
+    }
+    else {
+		res.redirect('/login');
+	}
+	res.end();
 });
  
-app.post('/save',(req, res) => { 
+app.post('/save',(req, res) => {
     let data = {emp_name: req.body.name, emp_salary: req.body.salary,emp_city: req.body.city};
     let sql = "INSERT INTO employee SET ?";
     let query = mysqlConn.query(sql, data,(err, results) => {
@@ -107,48 +186,8 @@ app.get('/delete/:id',(req, res) => {
 });
 
 
-
-
-
-
-
-//Get an employess
-// app.get('/employees',(req,res)=>{
-//     mysqlConn.query('SELECT * FROM employee',(err,rows,fields)=>{
-//         if(!err){
-//             //console.log(rows);
-//             res.send(rows);
-//         }
-//         else{
-//             console.log(err);
-//         }
-    
-//     })
-// });
-
-
-// // app.get('/employees/:id',(req,res)=>{
-// //     mysqlConn.query('SELECT * FROM employee where id =?',[req.params.id],(err,rows,fields)=>{
-// //         if(!err){
-// //             //console.log(rows);
-// //             res.send(rows);
-// //         }
-// //         else{
-// //             console.log(err);
-// //         }
-    
-// //     })
-// // });
-
-// app.delete('/employees/:id',(req,res)=>{
-//     mysqlConn.query('DELETE FROM employee where id =?',[req.params.id],(err,rows,fields)=>{
-//         if(!err){
-//             //console.log(rows);
-//             res.send(rows);
-//         }
-//         else{
-//             console.log(err);
-//         }
-    
-//     })
-// });
+app.get('/logOut',(req,res) =>{
+    req.session.destroy()
+    req.flash('success', 'Login Again Here')
+    res.redirect('/login')
+})
